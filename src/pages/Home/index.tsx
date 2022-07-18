e9d2d3e00be281516b257d0a58a1aaebaabe238b
sweetpea22 <cindychau24@me.com>
2022-07-18 09:33:45 -0400

    Setup 🤟🏼

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+export const Home: React.FC = () => {
+  return (
+    <Fragment>
+      <div className='container'>
+        <h1>Setup</h1>
+      </div>
+    </Fragment>
+  );
+};
