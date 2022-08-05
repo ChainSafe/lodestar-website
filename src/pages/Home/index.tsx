0f5471b2dffcc7b17175469457b9861d84ceac44
sweetpea22 <cindychau24@me.com>
2022-08-05 11:03:52 -0400

    Add header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,10 +9,12 @@ import { Support } from '../../components/Support';
 import { Team } from '../../components/Team';
 import { Contributors } from '../../components/Contributors';
 import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
 
 export const Home: React.FC = () => {
   return (
     <Fragment>
+      <Header />
       <Hero />
       <GeneralInfo />
       <Vision />
