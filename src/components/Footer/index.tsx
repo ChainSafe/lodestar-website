d40d7e2896fd2ffffa78d8209a2b9d15babf30df
sweetpea22 <cindychau24@me.com>
2022-12-15 12:56:41 -0500

    Update discord link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,45 +5,40 @@ import { Button } from '../Button';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="links">
-        <div className="links--column">
+    <footer className='footer'>
+      <div className='links'>
+        <div className='links--column'>
           <a
-            href="https://chainsafe.github.io/lodestar/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
+            href='https://chainsafe.github.io/lodestar/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer-link'>
             Docs
           </a>
           <a
-            href="https://chainsafe.typeform.com/lodestar?typeform-source=github.com"
-            className="newsletter-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            href='https://chainsafe.typeform.com/lodestar?typeform-source=github.com'
+            className='newsletter-link'
+            target='_blank'
+            rel='noopener noreferrer'>
             <Button dark>Subscribe</Button>
           </a>
-          <div className="icons">
+          <div className='icons'>
             <a
-              href="https://discord.gg/Qdf6GyNB5J"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+              href='https://discord.com/invite/xSAwrnCWcg'
+              target='_blank'
+              rel='noopener noreferrer'>
               <DiscordLogo size={28} />
             </a>
             <a
-              href="https://twitter.com/lodestar_eth"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+              href='https://twitter.com/lodestar_eth'
+              target='_blank'
+              rel='noopener noreferrer'>
               <TwitterLogo size={28} />
             </a>
             <a
-              href="https://github.com/ChainSafe/lodestar"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+              href='https://github.com/ChainSafe/lodestar'
+              target='_blank'
+              rel='noopener noreferrer'>
               <GithubLogo size={28} />
             </a>
           </div>
